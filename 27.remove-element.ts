0a74beeccd18c20c85df678feec6d24aee09b46c
Gressi-177 <vietdoan177@gmail.com>
2025-05-10 00:57:58 +0700

    feat:Add solutions for LeetCode problems 27 and 88
    - Implemented `removeElement` function to remove all instances of a specified value from an array.
    - Implemented `merge` function to merge two sorted arrays into the first array in-place.

---
+++ 27.remove-element.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=27 lang=typescript
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+function removeElement(nums: number[], val: number): number {
+  let k = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k++] = nums[i];
+    }
+  }
+  return k;
+}
+
+// @lc code=end
